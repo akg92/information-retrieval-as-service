a7ad658cc72947eade627e0ff4efc04160bb9cc0
agpandit <43184200+agpandit@users.noreply.github.com>
2019-05-05 00:34:22 -0500

    UI ( Not final - Partially complete )

---
+++ demo/demo/demo/static/main.js
@@ -0,0 +1,66 @@
+
+
+var populate = (data)=>{
+    let div = $(".result_cls");
+    div.empty();
+    var sample =  ` <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1"></h5>
+      <small class="text-muted"></small>
+    </div>
+  </a>`;
+    data = data['result']
+    for (let i in data){
+        let val = data[i];
+        let child = $($.parseHTML(sample));
+        child.find("h5").text(val.question);
+        child.find("small").text(val.score);
+        div.append(child);
+    }
+
+}
+
+var onenter = ()=>{
+
+    let data = $("#search_box").val();
+    data = {"query":data};
+    data = JSON.stringify(data);
+    //let algo_id=$(".dropdown .btn").text()||'conv1d';
+
+   // $.post("/matching/?algo_id="+algo_id,data).done((resp)=>{
+     //   resp=JSON.parse(resp);
+       // populate(resp);
+    //});
+    $.get('/search', {query:data}, function (data, textStatus, jqXHR) {
+    $('p').append(data.firstName);
+});
+
+
+};
+$(()=>{
+
+      $(".dropdown-menu li a").click(function(){
+  $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+  $(this).parents(".dropdown").find('.btn').val($(this).data('value'))});
+
+
+      $(".search_icon").click(function(){
+          onenter()
+      });
+
+    $(document).keypress(function(event){
+	
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $(document).keypress(function(event){
+	
+                var keycode = (event.keyCode ? event.keyCode : event.which);
+                if(keycode == '13'){
+                    onenter();	
+                }
+                
+            });
+        }
+        
+    });
+});
---
+++ demo/demo/rest/views.py
@@ -0,0 +1,81 @@
+
+import sys
+import os
+base_dir  = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),'models'))
+sys.path.append(base_dir)
+print('Base Dir {}'.format(base_dir))
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+#from infer import InferConv1d,InferCharEmbModel,InferXGBChar
+
+
+# Create your views here.
+
+question_list = questions = ["How is information retrieval course in tamu?","Information retrieval content","How are you today"]
+
+def test_hello(request):
+    return HttpResponse("Hello testing")
+
+
+def construct_response(data=None):
+
+    if not data:
+        data = [{"question":"How is IR course?","score":0.9},{"question":"Contents of IR course?","score":0.7}]
+
+    print('Result to json{}'.format(data))
+    resp = HttpResponse(json.dumps(data))
+    return resp
+
+def predict(algo_id,question):
+    global question_list
+    model = None
+    if algo_id=='conv1d':
+        model = InferConv1d()
+    elif algo_id =='xgboostchar':
+        model = InferXGBChar()
+    elif algo_id=='charlstm':
+        model = InferCharEmbModel()
+    result = model.predict(question,question_list)
+
+
+    return result
+
+
+@csrf_exempt
+def update_question_list(request):
+    global  question_list
+    body = request.body.decode("utf-8")
+    json_body = json.loads(body)
+    all_questions = json_body["questions"]
+    if request.method == 'POST':
+        question_list.extend(all_questions)
+    elif request.method=='PUT':
+        question_list.clear()
+        question_list.append(all_questions)
+
+
+
+@csrf_exempt
+def generate_prediction(request):
+    algo_id = request.GET.get('algo_id','conv1d')
+    body = request.body.decode("utf-8")
+    json_body = json.loads(body)
+    data = ""
+    if "query" in json_body:
+        data = json_body['query']
+    print("Data parsed{}".format(data))
+    print('Algo id{}'.format(algo_id))
+
+    ## call algorithm below
+
+    ## responese
+    data = predict(algo_id,data)
+    return construct_response(data)
+    
+
+
+
+
